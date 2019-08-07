370954f05b3fcfe7395476fc898923ac6644943f
Mukeshvenkat <mukiv0802@gmail.com>
2019-08-08 00:13:04 +0530

    fs module and export module

---
+++ NotesApp/notes-app/app.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt', 'First file writing application!');
+// fs.appendFileSync('notes.txt', '\n Data appended to the file');
+
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
